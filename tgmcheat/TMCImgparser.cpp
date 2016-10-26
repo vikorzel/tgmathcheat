3fd0c4cba0ac528208aeb7b27834c5f879884a38
vikorzel <hard.slot@gmail.com>
2016-10-26 22:48:05 +0300

    Added sample's cuter.

---
+++ tgmcheat/TMCImgparser.cpp
@@ -0,0 +1,6 @@
+#include "imgparser.h"
+
+imgparser::imgparser()
+{
+
+}
---
+++ tgmcheat/TMCImgparser.h
@@ -0,0 +1,11 @@
+#ifndef IMGPARSER_H
+#define IMGPARSER_H
+
+
+class imgparser
+{
+public:
+    imgparser();
+};
+
+#endif // IMGPARSER_H
